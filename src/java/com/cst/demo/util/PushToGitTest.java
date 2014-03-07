7874c262822a2b9e959c6152c1108f057c9b7ca9
liwj <road_ok@163.com>
2014-03-07 17:00:31 +0800

    创建新的类，提交上git

---
+++ src/java/com/cst/demo/util/PushToGitTest.java
@@ -0,0 +1,14 @@
+package com.cst.demo.util;
+
+/**
+ * <p>File Name : PushToGitTest.java</p>
+ * <p>Description : 创建一个新的class，测试提交到git</p>
+ * <p>Company : CST</p>
+ * <p>created : 2014年3月7日</p>	
+ * @author liwj
+ */
+public class PushToGitTest {
+	public static void main(String[] args) {
+		System.out.println("提交到git");
+	}
+}
